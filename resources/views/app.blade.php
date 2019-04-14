66ea30839cd58ac14db044cfa428fa656361e42b
Luis H <oneillvilla@gmail.com>
2019-04-14 05:52:19 -0430

    70

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,48 +4,51 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
-
-
         <!--CSS Bootstrap-->
-
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         
     </head>
     <body>
+        <!--NavBar-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-            
-                <img src="https://www.logocrea.com/2/wp-content/uploads/2016/07/hoja2.png" width="30" height="30" alt="">
-            
-            
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
                 
-                <ul class="navbar-nav ml-auto">
+                <img src="https://cdn.iconscout.com/icon/free/png-256/coding-581-896297.png" width="30" height="30" alt="">
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Portafolio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contáctame</a>
-                    </li>
-
+                <a class="navbar-brand" style="margin-left: 30px" href="#">Luis Hurtado</a>
                 
-            </span>
-
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
+                    
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Portafolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Portafolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contáctame</a>
+                        </li>
+                        
+                    </span>
+                </div>
+            </nav>
+            <div class="container">
+                @yield('content')
             </div>
-        </nav>
+            
+            
+        </body>
 
-
-        <div class="container">
-            @yield('content')
-        </div>
-        
-        
-    </body>
-</html>
+        <!--Bootstrap JS necesario para las funcionalidades activas de Bootstrap-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,47 +1,221 @@
 @extends('app')
-
 @section('title', 'Home')
-
 @section('content')
-
+<!--Header-->
 <div class="main-header">
 	
-	<div class="container">
-
+	<div class="container py-5">
 		<div class="row">
-
+			<div class="col-md-6 text-center align-self-center justify-content-center">
+				<h1>Desarrollador Web - Mobile</h1>
+				<br>
+				<h5>php con Laravel y Nativo (Android).</h5>
+				<br>
+				<button type="button" class="btn btn-outline-secondary btn-lg">Descargar CV</button>
+			</div>
 			<div class="col-md-6">
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				<br>
+				<img src="https://mfiles.alphacoders.com/675/thumb-675032.jpg" class="img-fluid rounded mx-auto d-block" alt="imagen_header">
+			</div>
+			
+		</div>
+		
+	</div>
+</div>
+<!-- Newsletter-->
+<section class="mx-auto py-5 bg-light">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 form-group mx-auto">
+				<input type="text" class="form-control" id="Name" placeholder="Name">
+			</div>
+			<div class="col-md-4 form-group mx-auto">
+				<input type="text" class="form-control" id="Name" placeholder="email">
+			</div>
+			<div class="col-md-4 mx-auto">
+				<button class="btn btn-outline-secondary btn-block">GO!</button>
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</section>
+<!-- Newsletter-->
+<section class="mx-auto py-5 text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="card border-secondary">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="card-link">Card link</a>
+						<a href="#" class="card-link">Another link</a>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-md-3">
+				<div class="card border-secondary">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="card-link">Card link</a>
+						<a href="#" class="card-link">Another link</a>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-md-3">
+				<div class="card border-secondary">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="card-link">Card link</a>
+						<a href="#" class="card-link">Another link</a>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-md-3">
+				<div class="card border-secondary">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="card-link">Card link</a>
+						<a href="#" class="card-link">Another link</a>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</section>
+<!--Por que este producto-->
+<section>
+	
+	<div class="container">
+		<div class="row">
+			<div class="my-5 mx-auto text-center bg-light">
+				<h3 class="my-3">¿Why this product?</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-				
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
-
-			<div class="col-md-6">
-
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				
-
-			</div>
-			
-
 		</div>
 		
-
 	</div>
-
-</div>
-
-
-
-@endsection
+	
+</section>
+<!--According-->
+<section>
+	
+	<div class="container text-center">
+		
+		
+		<div class="accordion" id="accordionExample">
+			<div class="card">
+				<div class="card-header" id="headingOne">
+					<h2 class="mb-0">
+					<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+					Collapsible Group Item #1
+					</button>
+					</h2>
+				</div>
+				<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+					<div class="card-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					</div>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-header" id="headingTwo">
+					<h2 class="mb-0">
+					<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+					Collapsible Group Item #2
+					</button>
+					</h2>
+				</div>
+				<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+					<div class="card-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					</div>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-header" id="headingThree">
+					<h2 class="mb-0">
+					<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+					Collapsible Group Item #3
+					</button>
+					</h2>
+				</div>
+				<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+					<div class="card-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+	<!--Tims-->
+	<section class="team text-center py-5 mx-auto">
+		
+		<div class="container bg-light">
+			<div class="row">
+				
+					<div class="col-md-3 mx-auto">
+						<div class="my-5 mx-auto text-center bg-light">
+							<div class="card">
+								<img src="http://pluspng.com/img-png/user-png-icon-male-user-icon-512.png" style="width: 50px; margin: 10px" class="card-img-top mx-auto" alt="...">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 mx-auto">
+						<div class="my-5 mx-auto text-center bg-light">
+							<div class="card">
+								<img src="http://pluspng.com/img-png/user-png-icon-male-user-icon-512.png" style="width: 50px; margin: 10px" class="card-img-top mx-auto" alt="...">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 mx-auto">
+						<div class="my-5 mx-auto text-center bg-light">
+							<div class="card">
+								<img src="http://pluspng.com/img-png/user-png-icon-male-user-icon-512.png" style="width: 50px; margin: 10px" class="card-img-top mx-auto" alt="...">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+					
+				</div>
+			</div>
+			
+		</div>
+		
+	</section>
+	@endsection
